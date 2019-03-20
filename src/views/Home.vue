a37c86b6af6a6863db08f81d95ca490dfb686c93
jak <467064378@qq.com>
2019-03-20 18:49:04 +0800

    新增电子书项目

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <div id="read"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Epub, { Book } from "epubjs";
+@Component({
+  components: {}
+})
+export default class Home extends Vue {
+  book: Book;
+  constructor(){
+    super();
+    this.book = Epub("/146678.epub");
+  }
+  mounted() {
+    console.log(this.book.renderTo);
+    this.book.renderTo("read", {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }).display();
+  }
+}
+</script>
